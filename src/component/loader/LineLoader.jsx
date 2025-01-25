269bd49efb6116a6b46312ad481bdae19de381da
Abhay gupta <hackyabhay@gmail.com>
2025-01-25 17:59:48 +0530

    udpated search

--- src/component/loader/LineLoader.jsx
+++ src/component/loader/LineLoader.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const LineLoader = () => {
   return (
-    <div className="relative w-full h-1 bg-gray-100 overflow-hidden">
-      <div className="absolute top-0 left-0 h-full w-1/3 bg-black animate-line-loader"></div>
+    <div className="absolute top-0 left-0 right-0 z-10">
+      <div className="relative w-full h-0.5 bg-gray-100 overflow-hidden">
+        <div className="absolute top-0 left-0 h-full w-1/3 bg-black animate-line-loader"></div>
+      </div>
     </div>
   );
 };
